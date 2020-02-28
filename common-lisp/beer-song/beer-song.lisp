31c98f7510f4c504539b854791d3e1aaa25122ae
Bradley M. Small <bradley_small@hotmail.com>
2020-02-28 06:20:07 -0500

    Wip 0 is not nul

--- common-lisp/beer-song/beer-song.lisp
+++ common-lisp/beer-song/beer-song.lisp
@@ -8,19 +8,17 @@
 (defun verse (beer-verse)
   "verse returns a single verse of the beer song."
   (cond
-    ((> beer-verse 2)  
-      (format nil "~A bottles of beer on the wall, ~A bottles of beer.~&~
-        Take one down and pass it around, ~A bottles of beer on the wall.~%"
-              beer-verse beer-verse (- beer-verse 1)))
+    ((> beer-verse 0)  
+      (format nil "~A bottle~:p of beer on the wall, ~A bottle~:p of beer.~&~
+        Take ~[it~:;one~] down and pass it around, ~:[no more~;~:*~A~] bottle~:p of beer on the wall.~%"
+              beer-verse beer-verse (- beer-verse 1) (- beer-verse 1)))
     ((= beer-verse 0)
       (format nil "No more bottles of beer on the wall, no more bottles of beer.~&~
         Go to the store and buy some more, 99 bottles of beer on the wall.~%"))
-    ((= beer-verse 1)
-      (format nil "1 bottle of beer on the wall, 1 bottle of beer.~&~
-        Take it down and pass it around, no more bottles of beer on the wall.~%"))
-    ((= beer-verse 2)
-      (format nil "2 bottles of beer on the wall, 2 bottles of beer.~&~
-        Take one down and pass it around, 1 bottle of beer on the wall.~%"))))
+    ;; ((= beer-verse 1)
+    ;;   (format nil "1 bottle of beer on the wall, 1 bottle of beer.~&~
+    ;;     Take it down and pass it around, no more bottles of beer on the wall.~%"))
+    ))
 
 (defun sing (begin-verse &optional (end-verse 0))
   "sing returns the beer song, from begin verse to the end verse"
