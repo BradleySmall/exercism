e5ed84fb83d6c4c9a6a6003e4ff1fcdabda9d8e1
Bradley M. Small <bradley_small@hotmail.com>
2020-04-01 00:19:24 -0400

    more scrabble refactoring

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,67 +2,34 @@ package scrabble
 
 import "unicode"
 
+func Tile2Score(letter rune) int {
+	var score = 0
+	switch unicode.ToUpper(letter) {
+	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		score = 1
+	case 'D', 'G':
+		score = 2
+	case 'B', 'C', 'M', 'P':
+		score = 3
+	case 'F', 'H', 'V', 'W', 'Y':
+		score = 4
+	case 'K':
+		score = 5
+	case 'J', 'X':
+		score = 8
+	case 'Q', 'Z':
+		score = 10
+	default:
+		score = 0
+	}
+	return score
+}
+
 // Score converts a string into its integer scrabble score.
 func Score(str string) int {
 	var accumulator = 0
 	for _, c := range str {
-		switch unicode.ToUpper(c) {
-		case 'D', 'G':
-			accumulator += 2
-		case 'B', 'C', 'M', 'P':
-			accumulator += 3
-		case 'F', 'H', 'V', 'W', 'Y':
-			accumulator += 4
-		case 'K':
-			accumulator += 5
-		case 'J', 'X':
-			accumulator += 8
-		case 'Q', 'Z':
-			accumulator += 10
-		default:
-			accumulator++
-		}
+		accumulator += Tile2Score(c)
 	}
 	return accumulator
 }
-
-/*
-
-Original
-=== RUN   TestScore
---- PASS: TestScore (0.00s)
-goos: linux
-goarch: amd64
-pkg: scrabble
-BenchmarkScore
-BenchmarkScore-8          658520              1815 ns/op
-    80 B/op            9 allocs/op
-PASS
-ok      scrabble        1.929s
-*/
-/*
-unicode.ToUpper()
-=== RUN   TestScore
---- PASS: TestScore (0.00s)
-goos: linux
-goarch: amd64
-pkg: scrabble
-BenchmarkScore
-BenchmarkScore-8          889408              1338 ns/op
-     0 B/op            0 allocs/op
-PASS
-ok      scrabble        1.891s
-*/
-/*
-switch/case
-=== RUN   TestScore
---- PASS: TestScore (0.00s)
-goos: linux
-goarch: amd64
-pkg: scrabble
-BenchmarkScore
-BenchmarkScore-8         2665728               443 ns/op
-     0 B/op            0 allocs/op
-PASS
-ok      scrabble        1.643s
-*/
