1a56e181327921eabc4e0a35d4f4d6f15a527c47
Bradley M. Small <bradley_small@hotmail.com>
2020-04-01 00:24:07 -0400

    scrabble score final optimization

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,34 +2,30 @@ package scrabble
 
 import "unicode"
 
-func Tile2Score(letter rune) int {
-	var score = 0
-	switch unicode.ToUpper(letter) {
-	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-		score = 1
-	case 'D', 'G':
-		score = 2
-	case 'B', 'C', 'M', 'P':
-		score = 3
-	case 'F', 'H', 'V', 'W', 'Y':
-		score = 4
-	case 'K':
-		score = 5
-	case 'J', 'X':
-		score = 8
-	case 'Q', 'Z':
-		score = 10
-	default:
-		score = 0
-	}
-	return score
-}
-
 // Score converts a string into its integer scrabble score.
 func Score(str string) int {
 	var accumulator = 0
-	for _, c := range str {
-		accumulator += Tile2Score(c)
+	for _, letter := range str {
+		var score = 0
+		switch unicode.ToUpper(letter) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score = 1
+		case 'D', 'G':
+			score = 2
+		case 'B', 'C', 'M', 'P':
+			score = 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score = 4
+		case 'K':
+			score = 5
+		case 'J', 'X':
+			score = 8
+		case 'Q', 'Z':
+			score = 10
+		default:
+			score = 0
+		}
+		accumulator += score
 	}
 	return accumulator
 }
